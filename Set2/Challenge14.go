81ff37d4b460d0aa54f07bb1a0f6f28aeb042131
C-Sto <7466346+C-Sto@users.noreply.github.com>
2018-07-29 22:24:25 +1000

    solve 14'

--- Set2/Challenge14.go
+++ Set2/Challenge14.go
@@ -20,26 +20,75 @@ What's harder than challenge #12 about doing this? How would you overcome that o
 
 Think "STIMULUS" and "RESPONSE".
 */
-var randomPrefix = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+var randomPrefix = []byte{1, 2}
 
 func Challenge14() {
 	key = cryptolib.RandomKey()
-	randomPrefix = cryptolib.RandomBytes()
+	//randomPrefix = cryptolib.RandomBytes()
+	blockSize := 16
+	padCount := 48
 	//Identify what a block of A's looks like (so we know where the injection is)
-	ct := Challenge14Oracle([]byte(strings.Repeat("A", 48)))
+	ct := Challenge14Oracle([]byte(strings.Repeat("A", padCount)))
 
-	knownBlock := []byte{}
-	if repeated, index := cryptolib.HasRepeatedBlocks(ct, 16); repeated {
-		knownBlock = cryptolib.Chunker(ct, 16)[index]
+	_, knownIndex := cryptolib.HasRepeatedBlocks(ct, 16)
+	//reduce the repeat count until the duplicate disappears
+	for { //go and it's stupid scopes
+		padCount--
+		ct = Challenge14Oracle([]byte(strings.Repeat("A", padCount)))
+
+		y, _ := cryptolib.HasRepeatedBlocks(ct, 16)
+		if !y || padCount < 1 {
+			break
+		}
+	}
+	//we know that two full blocks occur at current padcount+1, so the 'standing' prefix len will be
+	//(padcount+1)-(blocksize*2)
+	padCount++
+	standingPrefix := []byte(strings.Repeat("A", (padCount)-(16*2)))
+
+	unknown := len(cryptolib.Chunker(Challenge14Oracle([]byte(strings.Repeat("A", padCount))), 16)) - (knownIndex + 1)
+
+	//BEGIN COPYPASTAD CODE FROM 12
+	known := []byte{}
+	for blockIndex := knownIndex; blockIndex < unknown+1; blockIndex++ {
+		for unknownByteIndex := 0; unknownByteIndex < blockSize; unknownByteIndex++ {
+
+			//generate our checker block
+			checkerBlock := standingPrefix
+			//if we don't have enough known bytes, the checker will be padded with known bytes (A's)
+			lastIndex := len(known) - blockSize + 1
+			if len(known) < blockSize {
+				checkerBlock = append(checkerBlock, []byte(strings.Repeat("A", blockSize-len(known)-1))...)
+				lastIndex = 0
+			}
+			if len(known) > 0 {
+				substringKnown := known[lastIndex:]
+				checkerBlock = append(checkerBlock, substringKnown...)
+			}
+
+			for candidate := 0; candidate < 256; candidate++ {
+				//append to candidate
+				checkerBlock = append(checkerBlock, byte(candidate))
+
+				//if we need to add the padding bytes because we don't know enough yet, make it so
+				checkerBlock = append(checkerBlock, []byte(strings.Repeat("A", blockSize-unknownByteIndex-1))...)
+
+				ct := Challenge14Oracle(checkerBlock)
+				//fmt.Println(checkerBlock, "\n", cryptolib.Chunker(ct, blockSize)[1], cryptolib.Chunker(ct, blockSize)[blockIndex])
+				//check if our block[0] matches the target
+				if cryptolib.CompareBlocks(ct, blockSize, 1, blockIndex) {
+					fmt.Print(string(byte(candidate)))
+					known = append(known, byte(candidate))
+					break
+				}
+
+				//reset checkerboi
+				checkerBlock = checkerBlock[:(blockSize+len(standingPrefix))-1]
+			}
+
+		}
 	}
 
-	fmt.Println(knownBlock)
-	fmt.Println(cryptolib.Chunker(ct, 16))
-}
-
-//GetIndexOfBlock given a slice of byteslices, will find the index that the given block is at
-func GetIndexOfBlock(block []byte, blocks [][]byte) int {
-	return -1
 }
 
 func Challenge14Oracle(in []byte) []byte {
@@ -51,5 +100,6 @@ YnkK`
 	if err != nil {
 		panic(err)
 	}
-	return cryptolib.AESECBEncrypt(append(randomPrefix, append(in, decodedSecret...)...), key)
+	v := append(randomPrefix, append(in, decodedSecret...)...)
+	return cryptolib.AESECBEncrypt(v, key)
 }
